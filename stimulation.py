adae7242890f238879ab68d87fbfdcbee3264cf0
Eashan Sahai <esahai@sas.upenn.edu>
2024-02-27 12:12:18 -0700

    This is the stim code

---
+++ stimulation.py
@@ -0,0 +1,55 @@
+import time
+import os
+import clr
+
+from System import Action
+from System import *
+
+clr.AddReference(os.getcwd() + '\\..\\..\\McsUsbNet\\x64\\\McsUsbNet.dll')
+from Mcs.Usb import CMcsUsbListNet
+from Mcs.Usb import DeviceEnumNet
+
+from Mcs.Usb import CStg200xDownloadNet
+from Mcs.Usb import McsBusTypeEnumNet
+from Mcs.Usb import STG_DestinationEnumNet
+
+def PollHandler(status, stgStatusNet, index_list):
+    print('%x %s' % (status, str(stgStatusNet.TiggerStatus[0])))
+
+deviceList = CMcsUsbListNet(DeviceEnumNet.MCS_DEVICE_USB)
+
+print("found %d devices" % (deviceList.Count))
+
+for i in range(deviceList.Count):
+    listEntry = deviceList.GetUsbListEntry(i)
+    print("Device: %s   Serial: %s" % (listEntry.DeviceName,listEntry.SerialNumber))
+
+
+device = CStg200xDownloadNet();
+
+device.Stg200xPollStatusEvent += PollHandler;
+
+device.Connect(deviceList.GetUsbListEntry(0))
+
+voltageRange = device.GetVoltageRangeInMicroVolt(0);
+voltageResulution = device.GetVoltageResolutionInMicroVolt(0);
+currentRange = device.GetCurrentRangeInNanoAmp(0);
+currentResolution = device.GetCurrentResolutionInNanoAmp(0);
+
+print('Voltage Mode:  Range: %d mV  Resolution: %1.2f mV' % (voltageRange/1000, voltageResulution/1000.0))
+print('Current Mode:  Range: %d uA  Resolution: %1.2f uA' % (currentRange/1000, currentResolution/1000.0))
+
+channelmap = Array[UInt32]([1,0,0,0])
+syncoutmap = Array[UInt32]([1,0,0,0])
+repeat = Array[UInt32]([10,0,0,0])
+
+amplitude = Array[Int32]([-5000,5000,0]);
+duration = Array[UInt64]([100,100,1000000]);
+
+device.SetupTrigger(0, channelmap, syncoutmap, repeat)
+device.SetCurrentMode();
+device.PrepareAndSendData(0, amplitude, duration, STG_DestinationEnumNet.channeldata_current)
+device.SendStart(1) 
+time.sleep(10)
+
+device.Disconnect()
