4fb992e50f3314139fbf50502a3b8946b27b7b1e
Eashan Sahai <esahai@sas.upenn.edu>
2024-03-12 13:15:21 -0600

    changed up switching test code

--- switching_test.sh
+++ switching_test.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-curl -X GET "http://localhost:5000/api/set_all?signal=FFFFFFFFFFFFFFFF"
-sleep(1)
-curl -X GET "http://localhost:5000/api/set_all?signal=CAFFFFFFFFFFFFFF"
-curl -X GET "http://localhost:5000/api/set_all?signal=FFCAFFFFFFFFFFFF"
+
+# Loop 20 times
+for i in {1..20}; do
+  # Base state
+  curl -X GET "http://localhost:5000/api/set_all?signal=GGGGGGGGGGGGGGGG"
+  
+  # First modification
+  curl -X GET "http://localhost:5000/api/set_all?signal=CGGGGGGGGGGGGGGG"
+  
+  # Second modification
+  curl -X GET "http://localhost:5000/api/set_all?signal=GGCGGGGGGGGGGGGG"
+  
+  # Rest for 2 seconds
+  sleep 2
+done
